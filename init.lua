7819b319a6bc2f59f9ef90b91fddddc1f7dfd256
mmitten <mmitten@gmail.com>
2024-10-23 06:41:34 -0400

    Fixed a boo boo

--- init.lua
+++ init.lua
@@ -618,7 +618,6 @@ require('lazy').setup({
         tailwindcss = {
           filetypes = { 'html', 'templ' },
         },
-        tsserver = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
